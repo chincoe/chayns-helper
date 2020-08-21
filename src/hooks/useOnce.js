33a18ecf5184a64f67fe6753ffb26e0a977bf7a0
Christian Nagel <christian.nagel@tobit.com>
2020-08-21 17:34:37 +0200

    :truck: Rename useTest to useOnce

--- src/hooks/useOnce.js
+++ src/hooks/useOnce.js
@@ -4,6 +4,6 @@ import { useEffect } from 'react';
  * Use this hook somewhere in a react component if you just want to test a few lines of code
  * @param {function} fn
  */
-const useTest = (fn) => useEffect(fn, []);
+const useOnce = (fn) => useEffect(fn, []);
 
-export default useTest;
+export default useOnce;
