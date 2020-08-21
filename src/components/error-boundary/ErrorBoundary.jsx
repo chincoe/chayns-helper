4ac20d18a2b12f3039f6494b2169351e55a172f9
Christian Nagel <christian.nagel@tobit.com>
2020-08-21 17:33:05 +0200

    :bug: Fix errorBoundary

--- src/components/error-boundary/ErrorBoundary.jsx
+++ src/components/error-boundary/ErrorBoundary.jsx
@@ -6,7 +6,7 @@ import CenteredButtonContainer from '../containers/CenteredButtonContainer';
 import { chaynsHelperConfig } from '../../chaynsHelperConfig';
 
 /**
- * An error-boundary.
+ * An ErrorBoundary.
  * Wrap your application in this. If an error occurs during render that causes react to fail rendering, this component
  * will catch the error, log it and display a fallback instead. The default fallback is a warning content card with a
  * generic error message, error details and reload button.
@@ -33,7 +33,7 @@ class ErrorBoundary extends React.Component {
     componentDidCatch(error, errorInfo) {
         // You can also log the error to an error reporting service
         chaynsHelperConfig.getLogger().error({
-            message: '[error-boundary] Unexpected react error',
+            message: '[ErrorBoundary] Unexpected react error',
             data: { errorInfo }
         }, error);
     }
@@ -44,38 +44,30 @@ class ErrorBoundary extends React.Component {
         const FallbackComponent = fallback;
         if (state.hasError) {
             // You can render any custom fallback UI
-            return fallback ? (
-                <div className="ErrorBoundary">
-                    <div className="ErrorBoundary__content__card content__card content__card--warning">
-                        <h2>Es ist ein Fehler aufgetreten</h2>
-                        <p>Wir sind bereits davon informiert und beheben den Fehler so schnell wie möglich.</p>
-                        <p>
-                            {`Fehler: ${state.error.toString()}`}
-                        </p>
-                        <CenteredButtonContainer>
-                            <Button
-                                onClick={() => {
-                                    this.setState({
-                                        hasError: false,
-                                        error: null
-                                    });
-                                }}
-                            >
-                                Nochmal versuchen
-                            </Button>
-                            <Button onClick={() => {
-                                chayns.appendUrlParameter({ nocache: true }, true);
-                                // eslint-disable-next-line no-restricted-globals
-                                location.reload();
-                            }}
-                            >
-                                Neu Laden
-                            </Button>
-                        </CenteredButtonContainer>
-                    </div>
-                </div>
-            ) : (
-                       <FallbackComponent error={state.error}/>
+            return fallback
+                   ? (<FallbackComponent error={state.error}/>)
+                   : (
+                       <div className="ErrorBoundary">
+                           <div className="ErrorBoundary__content__card content__card content__card--warning">
+                               <h2>Es ist ein Fehler aufgetreten</h2>
+                               <p>Wir sind bereits davon informiert und beheben den Fehler so schnell wie möglich.</p>
+                               {!chaynsHelperConfig.live && (
+                                   <p>
+                                       {`Fehler: ${state.error.toString()}`}
+                                   </p>
+                               )}
+                               <CenteredButtonContainer>
+                                   <Button onClick={() => {
+                                       chayns.appendUrlParameter({ nocache: true }, true);
+                                       // eslint-disable-next-line no-restricted-globals
+                                       location.reload();
+                                   }}
+                                   >
+                                       Neu Laden
+                                   </Button>
+                               </CenteredButtonContainer>
+                           </div>
+                       </div>
                    );
         }
 
