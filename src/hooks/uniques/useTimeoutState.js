9534cd0590e1a568f4431e63e2fb844391780dfe
Christian Nagel <christian.nagel@tobit.com>
2020-09-07 11:37:50 +0200

    :pencil: Use manual default values for functions

--- src/hooks/uniques/useTimeoutState.js
+++ src/hooks/uniques/useTimeoutState.js
@@ -6,10 +6,10 @@ import useUniqueTimeout from './useUniqueTimeout';
  * useState that will call onChange only after no change has been made for a time equal to timeout ms
  * @param {*} initialValue
  * @param {function(value: *)} onChange
- * @param {number} timeout
+ * @param {number} [timeout=500]
  * @return {[*, function]}
  */
-const useTimeoutState = (initialValue, onChange, timeout = 500) => {
+const useTimeoutState = (initialValue, onChange, timeout) => {
     const [state, setState] = useState(initialValue);
     const [previousState, setPreviousState] = useState(initialValue);
     const setStateTimeout = useUniqueTimeout();
@@ -17,12 +17,12 @@ const useTimeoutState = (initialValue, onChange, timeout = 500) => {
     useEffect(() => {
         const t = setStateTimeout((
             chayns.utils.isString(state) || chayns.utils.isString(previousState)
-                ? `${state}` === `${previousState}`
-                : shallowEqual(state, previousState)
+            ? `${state}` === `${previousState}`
+            : shallowEqual(state, previousState)
         ) || setTimeout(() => {
             setPreviousState(state);
             onChange(state);
-        }, timeout));
+        }, (timeout ?? 500)));
         return () => {
             clearTimeout(t);
         };
