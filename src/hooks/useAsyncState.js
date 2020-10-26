9f8c55a808274c9eab2180dc38c5d809bbcaf320
Christian Nagel <christian.nagel@tobit.com>
2020-10-26 15:25:48 +0100

    :bug: Add default case for useAsyncState

--- src/hooks/useAsyncState.js
+++ src/hooks/useAsyncState.js
@@ -5,8 +5,8 @@ import { useState, useCallback } from 'react';
  *  - can call setState(Promise) and this hook will resolve it
  *  - can pass an async setter function to setState(), e.g. setState(async (prevState) => {....})
  * @template T
- * @param {T|Promise<T>} [initialState]
- * @returns {[T, function(T|Promise<T>|function(T): T|function(T): Promise<T>)]} - [state, setState]
+ * @param {?T|Promise<T>} [initialState]
+ * @returns {[?T|Promise<T>, function(T|Promise<T>|function(T=): T|function(T=): Promise<T>) : Promise<undefined>]} - [state, setState]
  */
 export default function useAsyncState(initialState) {
     const [state, setState] = useState(initialState);
@@ -14,7 +14,7 @@ export default function useAsyncState(initialState) {
         Promise.resolve(initialState).then(setState);
     }
 
-    const setter = useCallback((newValue) => {
+    const setter = useCallback((newValue) => new Promise((r) => {
         if (chayns.utils.isPromise(newValue)) {
             Promise.resolve(newValue).then(setState);
         } else if (chayns.utils.isFunction(newValue)) {
@@ -27,13 +27,20 @@ export default function useAsyncState(initialState) {
             prevStatePromise.then((prevState) => {
                 const nextValue = newValue(prevState);
                 if (chayns.utils.isPromise(nextValue)) {
-                    Promise.resolve(nextValue).then((res) => setState(res));
+                    Promise.resolve(nextValue).then((res) => {
+                        setState(res);
+                        r();
+                    });
                 } else {
                     setState(nextValue);
+                    r();
                 }
             });
+        } else {
+            setState(newValue);
+            r();
         }
-    }, []);
+    }), []);
 
     return [state, setter];
 }
