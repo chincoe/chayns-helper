9e16b91a532efcd1e418e980d522a54b88f05108
Christian Nagel <christian.nagel@tobit.com>
2020-12-10 09:16:48 +0100

    :wrench: Simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
-module.exports = (api) => {
-    api.cache(true);
-
-    return { presets: ['./babelPreset.js'] };
-};
+module.exports = {
+    plugins: [
+        ["const-enum", { "transform": "constObject" }],
+        '@babel/plugin-proposal-logical-assignment-operators',
+        '@babel/plugin-proposal-numeric-separator',
+        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+        '@babel/plugin-proposal-throw-expressions',
+        'optimize-clsx',
+    ],
+}
