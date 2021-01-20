918daecf9cdbf96a55e8a83369e8b1f22861fa0e
Christian Nagel <christian.nagel@tobit.com>
2021-01-20 12:05:47 +0100

    :hammer: Edit publish scripts

--- commit.bat
+++ commit.bat
@@ -4,5 +4,6 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`npm view chayns-helper version`) DO (
 SET pkgversion=%%F
 )
 SET str=%1
-SET depmessage=%str:#pkgversion#=!pkgversion!%
-npm deprecate chayns-helper@%pkgversion% %depmessage%
+git commit -m ":bookmark: %str% %pkgversion%"
+git push
+git push --tags
