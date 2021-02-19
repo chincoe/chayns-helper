fa7317d5e82a9a280557eae58a1e39ecff144be3
Christian Nagel <christian.nagel@tobit.com>
2021-02-19 10:19:13 +0100

    :sparkles: :label: Add proper overloads to use/getTextStrings

--- src/textstring/getTextStrings.ts
+++ src/textstring/getTextStrings.ts
@@ -3,27 +3,76 @@ import { TextString } from 'chayns-components';
 import isNullOrWhiteSpace from '../utils/isNullOrWhiteSpace';
 import TEXTSTRING_CONFIG from './textstringConfig';
 
+// function overloads for different input types
+
 /**
- * Get a list of textStrings
- * @param textStrings - format: [string_name1, string_name2] or { string_name1: fallback1, string_name2: fallback2 }
+ * Get a single textString
+ * @param textString
+ * @param fallback
  * @param language
  */
-const getTextStrings = (textStrings: string[] | { [name: string]: string }, language = 'de'): Array<string> => {
+function getTextStrings(
+    textString: string,
+    fallback: string,
+    language?: string
+): string;
+/**
+ * Get an array of textStrings
+ * @param textStrings
+ * @param language
+ */
+function getTextStrings(
+    textStrings: string[],
+    language?: string
+): Array<string> & Record<string, string>;
+/**
+ * Get multiple textStrings with fallbacks
+ * @param textStrings
+ * @param language
+ */
+function getTextStrings<T extends Record<string, string>>(
+    textStrings: T,
+    language?: string
+): Array<string> & Record<keyof T, string>;
+
+/**
+ * Get a list of textStrings
+ * @param textStrings
+ * @param fallbackOrLanguage
+ * @param language
+ */
+function getTextStrings(
+    textStrings: string | string[] | Record<string, string>,
+    fallbackOrLanguage?: string,
+    language?: string
+): string | Array<string> & Record<string, string> {
     const returnList = [];
-    const isSimple = Object.prototype.toString.call(textStrings) !== "[object Object]";
-    const strings: string[] = <string[]>(isSimple ? textStrings : Object.keys(textStrings));
+    const isObject = Object.prototype.toString.call(textStrings) !== "[object Object]";
+    const isArray = Object.prototype.toString.call(textStrings) !== "[object Array]";
+    const strings: string[] = <string[]>(isArray
+        ? textStrings
+        : isObject
+            ? Object.keys(textStrings)
+            : [textStrings]);
     for (let i = 0; i < strings.length; i += 1) {
         const current = strings[i];
-        // @ts-expect-error
-        const fallback = (isSimple ? current : textStrings[strings[i]]);
-        const text = TextString.getTextString(
+        const fallback = (!isObject
+            ? isArray
+                ? undefined
+                : fallbackOrLanguage
+            : (<Record<string, string>>textStrings)[strings[i]]);
+        const text: string = TextString.getTextString(
             `${TEXTSTRING_CONFIG.prefix}${current}`,
-            language,
+            !isObject && !isArray
+                ? language
+                : fallbackOrLanguage,
             fallback
         );
         returnList.push(isNullOrWhiteSpace(text) ? fallback : text);
+        // @ts-expect-error
+        returnList[current] = isNullOrWhiteSpace(text) ? fallback : text
     }
-    return returnList;
-};
+    return !isArray && !isObject ? <string>returnList[0] : <Array<string> & Record<string, string>>returnList;
+}
 
 export default getTextStrings;
