15c5a1fb77dcce77e4cab8f4260c26847acc51a6
Christian Nagel <christian.nagel@tobit.com>
2021-04-12 16:55:36 +0200

    :bug: Add null value hadnling to setViewMode

--- src/functions/chaynsCalls/setViewMode.ts
+++ src/functions/chaynsCalls/setViewMode.ts
@@ -3,7 +3,7 @@
  */
 const setViewMode = (exclusive: boolean, tryFullBrowserWidth?: boolean): Promise<void> => {
     let viewMode = (tryFullBrowserWidth ?? false) ? 4 : (exclusive ? 2 : 0);
-    if ((tryFullBrowserWidth ?? false) && !chayns.env.site.disposition.contentWide) {
+    if ((tryFullBrowserWidth ?? false) && !chayns.env.site?.disposition?.contentWide) {
         // eslint-disable-next-line no-console
         console.warn('[SetViewMode] Cannot use wide view on this layout. Layout with wide content required');
         viewMode = 2;
